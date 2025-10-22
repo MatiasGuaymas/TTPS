4a9ce5f634776ce3e2e168882aa757e97486261c
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-10-22 15:53:01 -0300

    Agregado metodos a UsuarioDAO

--- src/main/java/io/github/vicen621/volveacasa/persistence/dao/UsuarioDAO.java
+++ src/main/java/io/github/vicen621/volveacasa/persistence/dao/UsuarioDAO.java
@@ -2,8 +2,15 @@ package io.github.vicen621.volveacasa.persistence.dao;
 
 import io.github.vicen621.volveacasa.entities.Usuario;
 
+import java.util.List;
+
 public interface UsuarioDAO extends GenericDAO<Usuario> {
     Usuario getByEmail(String mail);
     void disableUser(Usuario usuario);
     void disableUser(Long id);
+    void enableUser(Usuario usuario);
+    void enableUser(Long id);
+    List<Usuario> getByBarrio(String barrio);
+    List<Usuario> getByCiudad(String ciudad);
+    List<Usuario> getByRol(Usuario.Rol rol);
 }
--- src/main/java/io/github/vicen621/volveacasa/persistence/dao/hibernate/UsuarioDAOHibernate.java
+++ src/main/java/io/github/vicen621/volveacasa/persistence/dao/hibernate/UsuarioDAOHibernate.java
@@ -4,7 +4,10 @@ import io.github.vicen621.volveacasa.entities.Usuario;
 import io.github.vicen621.volveacasa.persistence.EntityManagerSingleton;
 import io.github.vicen621.volveacasa.persistence.dao.UsuarioDAO;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class UsuarioDAOHibernate extends GenericDAOHibernate<Usuario> implements UsuarioDAO {
 
@@ -19,16 +22,68 @@ public class UsuarioDAOHibernate extends GenericDAOHibernate<Usuario> implements
             TypedQuery<Usuario> query = em.createQuery(jpql, getEntityClass());
             query.setParameter("email", mail);
             return query.getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
         }
     }
 
     @Override
     public void disableUser(Usuario usuario) {
-        // TODO: Implementar
+        if (usuario != null) {
+            usuario.setHabilitado(false);
+            this.update(usuario);
+        }
     }
 
     @Override
     public void disableUser(Long id) {
-        // TODO: Implementar
+        Usuario usuario = this.get(id);
+        this.disableUser(usuario);
     }
+
+    @Override
+    public void enableUser(Usuario usuario) {
+        if (usuario != null) {
+            usuario.setHabilitado(true);
+            this.update(usuario);
+        }
+    }
+
+    @Override
+    public void enableUser(Long id) {
+        Usuario usuario = this.get(id);
+        this.enableUser(usuario);
+    }
+
+    @Override
+    public List<Usuario> getByBarrio(String barrio) {
+        try (EntityManager em = EntityManagerSingleton.getInstance().createEntityManager()) {
+            String jpql = "SELECT e FROM " + getEntityClass().getSimpleName() + " e WHERE e.barrio = :barrio";
+            TypedQuery<Usuario> query = em.createQuery(jpql, getEntityClass());
+            query.setParameter("barrio", barrio);
+            return query.getResultList();
+        }
+    }
+
+    @Override
+    public List<Usuario> getByCiudad(String ciudad) {
+        try (EntityManager em = EntityManagerSingleton.getInstance().createEntityManager()) {
+            String jpql = "SELECT e FROM " + getEntityClass().getSimpleName() + " e WHERE e.ciudad = :ciudad";
+            TypedQuery<Usuario> query = em.createQuery(jpql, getEntityClass());
+            query.setParameter("ciudad", ciudad);
+            return query.getResultList();
+        }
+    }
+
+    @Override
+    public List<Usuario> getByRol(Usuario.Rol rol) {
+        try (EntityManager em = EntityManagerSingleton.getInstance().createEntityManager()) {
+            String jpql = "SELECT e FROM " + getEntityClass().getSimpleName() + " e WHERE e.rol = :rol";
+            TypedQuery<Usuario> query = em.createQuery(jpql, getEntityClass());
+            query.setParameter("rol", rol);
+            return query.getResultList();
+        }
+    }
+
+
 }
