bb5d760da00cd4c0eb5f4e89f863344adff23405
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-11-02 22:19:31 -0300

    Arreglado tests

--- src/test/java/io/github/vicen621/volveacasa/persistence/controllers/UserRestControllerTest.java
+++ src/test/java/io/github/vicen621/volveacasa/persistence/controllers/UserRestControllerTest.java
@@ -1,43 +1,53 @@
 package io.github.vicen621.volveacasa.persistence.controllers;
 
 import io.github.vicen621.volveacasa.controllers.UserRestController;
-import io.github.vicen621.volveacasa.persistence.dao.hibernate.UsuarioDAOHibernate;
+import io.github.vicen621.volveacasa.persistence.dao.UsuarioDAO;
 import io.github.vicen621.volveacasa.persistence.entities.Usuario;
-import io.github.vicen621.volveacasa.persistence.dao.hibernate.UsuarioDAOHibernate;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = UserRestController.class)
-@WebAppConfiguration
-@ComponentScan(basePackages = "io.github.vicen621.volveacasa.persistence.dao.hibernate")
-public class UserRestControllerTest extends BaseControllerTest{
-    @Autowired
-    private  WebApplicationContext webApplicationContext;
-    @Autowired
-    private UsuarioDAOHibernate usuarioDAO;
+@ExtendWith(MockitoExtension.class)
+public class UserRestControllerTest extends BaseControllerTest {
+    @Mock
+    private UsuarioDAO usuarioDAO;
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void init() {
         createContext();
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @BeforeEach
+    void setup() {
         cleanDatabase();
+        mockMvc = MockMvcBuilders.standaloneSetup(new UserRestController(usuarioDAO)).build();
+    }
+
+    @Test
+    void listAllUsersOrderByName_whenEmpty_returnsNoContent() throws Exception {
+        when(usuarioDAO.getAll("nombre")).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/users").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void listAllUsersOrderByName_whenUsersExist_returnsOkAndList() throws Exception {
         Usuario usuario1 = Usuario.builder()
                 .nombre("Juan")
                 .apellidos("Perez")
@@ -60,15 +70,15 @@ public class UserRestControllerTest extends BaseControllerTest{
                 .latitud(-34.6037f)
                 .longitud(-58.3816f)
                 .build();
-        usuarioDAO.persist(usuario1);
-        usuarioDAO.persist(usuario2);
-    }
+        List<Usuario> users = Arrays.asList(usuario1, usuario2);
 
-    @Test
-    void testListAllUsersOrderByName() throws Exception{
-        mockMvc.perform(get("/users"))
+        when(usuarioDAO.getAll("nombre")).thenReturn(users);
+
+        mockMvc.perform(get("/users")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].nombre").value("Ana"))
-                .andExpect(jsonPath("$[1].nombre").value("Juan"));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].nombre").value("Juan"))
+                .andExpect(jsonPath("$[1].nombre").value("Ana"));
     }
 }
