8a2ac024599e289018319d4290255d4e8bfecaa0
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-12-12 18:31:41 -0300

    Agregado filtro de mascotas

--- backend/src/main/java/io/github/grupo01/volve_a_casa/persistence/repositories/PetRepository.java
+++ backend/src/main/java/io/github/grupo01/volve_a_casa/persistence/repositories/PetRepository.java
@@ -3,10 +3,11 @@ package io.github.grupo01.volve_a_casa.persistence.repositories;
 import io.github.grupo01.volve_a_casa.persistence.entities.Pet;
 import io.github.grupo01.volve_a_casa.persistence.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface PetRepository extends JpaRepository<Pet, Long> {
+public interface PetRepository extends JpaRepository<Pet, Long>, JpaSpecificationExecutor<Pet> {
     List<Pet> findAllByStateInOrderByLostDate(Pet.State... states);
     List<Pet> findAllByCreator(User creator);
 }
