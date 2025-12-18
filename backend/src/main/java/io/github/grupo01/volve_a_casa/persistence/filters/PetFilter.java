286ad535940dd0b9e81f7f4b04a1b32b27111cdb
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-12-18 12:13:19 -0300

    Arreglado filtros en el backend

--- backend/src/main/java/io/github/grupo01/volve_a_casa/persistence/filters/PetFilter.java
+++ backend/src/main/java/io/github/grupo01/volve_a_casa/persistence/filters/PetFilter.java
@@ -3,27 +3,26 @@ package io.github.grupo01.volve_a_casa.persistence.filters;
 import java.time.LocalDate;
 
 import io.github.grupo01.volve_a_casa.persistence.entities.Pet;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class PetFilter {
     private String name;
     private Pet.State state;
     private Pet.Type type;
-    private Pet.Size size;
+    private Pet.Size petSize;
     private String color;
     private String race;
-    private float weightMin;
-    private float weightMax;
+    private Float weightMin;
+    private Float weightMax;
     private LocalDate initialLostDate;
     private LocalDate finalLostDate;
-    //agrego atributos para filtrar por barrio
+    // filtro por barrio
     private Float userLatitude;
     private Float userLongitude;
     private Float maxDistanceInKm;
-
-
-
 }
