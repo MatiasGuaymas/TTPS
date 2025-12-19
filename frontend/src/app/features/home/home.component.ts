5335ee60260404f4bfe9e6811ae42da868e6c275
Matheo Joaquín Lamiral <matheolamiral05@gmail.com>
2025-12-18 22:49:33 -0300

    agregado top 5 al home

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,38 +1,58 @@
 import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common"; 
 import { UserCardComponent } from "../../shared/components/userCard/userCard.component";
 import { CarouselComponent } from "../../shared/components/carousel/carousel.component";
 import { MarkerInfo, MapComponent } from '../../shared/components/map/map'; 
 import { SightingService } from "../../core/services/sigthing.service";
+import { UserService } from "../../core/services/user.service";
 import { SightingResponse } from "../../core/models/sighting.models";
+import { UserProfile } from "../../core/models/user.models"; 
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.component.html',
-    imports: [CarouselComponent, UserCardComponent, MapComponent], 
+    imports: [CommonModule, CarouselComponent, UserCardComponent, MapComponent], 
 })
 export class HomeComponent implements OnInit {
     sightings: SightingResponse[] = [];
+    topUsers: UserProfile[] = []; 
     title = '¿Donde estás? Volvé a casa';
     public sightingsLocationList: MarkerInfo[] = [];
 
-    constructor(private sightingService: SightingService) {}
+    constructor(
+        private sightingService: SightingService,
+        private userService: UserService
+    ) {}
 
     ngOnInit() {
-        console.log('HomeComponent ngOnInit - solicitando avistamientos');
+        this.loadSightings();
+        this.loadTopUsers(); 
+    }
+
+    loadSightings() {
         this.sightingService.getAllSightings().subscribe({
             next: (response) => {
-                console.log('Avistamientos recibidos:', response.length, 'items');
                 this.sightings = response;
                 this.sightingsLocationList = response.map(sighting => ({
                     latitude: sighting.latitude,
                     longitude: sighting.longitude,
                     popupText: `<b>Avistamiento</b><br>${new Date(sighting.date).toLocaleDateString()}`
                 }));
-                console.log('sightingsLocationList actualizado:', this.sightingsLocationList.length, 'marcadores');
             },
             error: (error) => {
                 console.error('Error cargando avistamientos:', error);
                 this.sightingsLocationList = [];
+            }
+        });
+    }
+
+    loadTopUsers() {
+        this.userService.getAllUsersFiltered({}, 0, 5, 'points', 'DESC').subscribe({
+            next: (users) => {
+                this.topUsers = users;
+            },
+            error: (err) => {
+                console.error('Error cargando Top 5:', err);
             }
         });
     }
